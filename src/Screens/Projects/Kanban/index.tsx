5b3fd005c836d0300367b8559baa746ccc7c953c
无恙 <524867395@qq.com>
2021-06-22 17:13:20 +0800

    style: 优化看板样式

--- src/Screens/Projects/Kanban/index.tsx
+++ src/Screens/Projects/Kanban/index.tsx
@@ -1,24 +1,38 @@
+import { Spin } from "antd";
 import { ScreenContainer } from "components/lib";
 import React from "react";
 import styled from "styled-components";
 import { useDoucmentTitle } from "utils";
 import { useKanbans } from "utils/kanban";
+import { useTasks } from "utils/task";
 import KanbanColumn from "./KanbanColumn";
 import SearchPanel from "./SearchPanel";
-import { useKanbanSearchParams, useProjectInUrl } from "./util";
+import {
+  useKanbanSearchParams,
+  useProjectInUrl,
+  useTasksSearchParams,
+} from "./util";
 
 export default function Kanban() {
   useDoucmentTitle("看板列表");
   const { data: project } = useProjectInUrl();
-  const { data: kanbans } = useKanbans(useKanbanSearchParams());
+  const { data: kanbans, isLoading: kanbanLoading } = useKanbans(
+    useKanbanSearchParams()
+  );
+  const { isLoading: taskLoading } = useTasks(useTasksSearchParams());
+  const isLoading = kanbanLoading || taskLoading;
   return (
     <ScreenContainer>
       <h1>{project?.name}看板</h1>
       <SearchPanel />
       <ColumnsContainer>
-        {kanbans?.map((kanban) => (
-          <KanbanColumn key={kanban.id} kanban={kanban} />
-        ))}
+        {isLoading ? (
+          <Spin tip="Loading..." />
+        ) : (
+          kanbans?.map((kanban) => (
+            <KanbanColumn key={kanban.id} kanban={kanban} />
+          ))
+        )}
       </ColumnsContainer>
     </ScreenContainer>
   );
@@ -26,6 +40,12 @@ export default function Kanban() {
 
 export const ColumnsContainer = styled("div")`
   display: flex;
-  /* justify-content: space-between; */
   flex: 1;
+  .ant-spin {
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+  }
 `;
