97dc74f51cfa07cf2e9f7ba2ead5ffc5e4657c54
无恙 <524867395@qq.com>
2021-07-01 10:52:34 +0800

    feat: 代码分割

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,21 @@
+import React from "react";
 import { useAuth } from "context/auth-context";
-import Authenticated from "Authenticated";
-import Unauthenticated from "Screens/Unauthenticated";
 import ErrorBoundary from "components/ErrorBoundary";
-import { FullPageErrorFallback } from "components/lib";
+import { FullPageErrorFallback, FullPageLoading } from "components/lib";
 // import {ErrorBoundary} from 'react-error-boundary'
 import "App.css";
+
+const Authenticated = React.lazy(() => import("Authenticated"));
+const Unauthenticated = React.lazy(() => import("Screens/Unauthenticated"));
 
 function App() {
   const { user } = useAuth();
   return (
     <div className="App">
       <ErrorBoundary fallbackRender={FullPageErrorFallback}>
-        {user ? <Authenticated /> : <Unauthenticated />}
+        <React.Suspense fallback={FullPageLoading}>
+          {user ? <Authenticated /> : <Unauthenticated />}
+        </React.Suspense>
       </ErrorBoundary>
     </div>
   );
